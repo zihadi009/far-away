67f093d86ee87176fdc3b02e2194f3d374b673a3
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-03-03 12:00:29 +0600

    on submit handler added and prevented the page reload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 
 const App = () => {
   return (
     <div>
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 };
@@ -12,7 +22,59 @@ const App = () => {
 function Logo() {
   return (
     <h1>🌴 FAR AWAY 💼</h1>
+  );
+};
+
+function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1)
+          .map((num) => (
+            <option value={num} key={num}>{num}</option>
+          ))}
+      </select>
+      <input type="text" placeholder='Item...' />
+      <button>Add</button>
+    </form>
   )
 }
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => <Item item={item} key={item.id} />)}
+      </ul>
+    </div>
+  );
+};
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed X</em>
+    </footer>
+  )
+}
+
+
 
 export default App;
