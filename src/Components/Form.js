f79d021db782ce6407fe438541d2e1b4e5a06741
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-03-20 01:00:31 +0600

    title updated

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -12,7 +12,6 @@ export function Form({ onAddItems }) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        // console.log(e);
         if (!description) return;
         const newItem = { id: Date.now(), description, quantity, packed: false };
         onAddItems(newItem);
@@ -20,18 +19,17 @@ export function Form({ onAddItems }) {
         setDescription("");
         setQuantity(1);
     }
-
     return (
         <form className='add-form' onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-            <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+            <select value={quantity} onChange={(e) => setQuantity(Number(() => e.target.value))}>
                 {Array.from({ length: 20 }, (_, i) => i + 1)
                     .map((num) => (
                         <option value={num} key={num}>{num}</option>
                     ))}
             </select>
-            <input type="text" placeholder='Item...' value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input type="text" placeholder='Item...' value={description} onChange={(e) => setDescription(() => e.target.value)} />
             <button>Add</button>
         </form>
     );
-}
+};
